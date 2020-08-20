d3f54dc38ec50ef4e7d1566de29bcfe25bf81c09
Andrey <arsenal1251@mail.ru>
2020-08-20 13:25:20 +0300

    gitignore file was changed

--- src/test/java/com/Andrey/CustomPizza/CustomPizzaApplicationTests.java
+++ src/test/java/com/Andrey/CustomPizza/CustomPizzaApplicationTests.java
@@ -3,22 +3,52 @@ package com.Andrey.CustomPizza;
 import com.Andrey.CustomPizza.controller.UserController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class CustomPizzaApplicationTests {
+@AutoConfigureMockMvc
+public class CustomPizzaApplicationTests {
 
-    private final UserController userController;
 
-    CustomPizzaApplicationTests(UserController userController) {
-        this.userController = userController;
+    public CustomPizzaApplicationTests() {
+    }
+
+    @Autowired
+    public UserController userController;
+
+    @Autowired
+    public MockMvc mockMvc;
+
+    @Test
+    public void testController() throws Exception{
+        assertThat(userController).isNotNull();
     }
 
     @Test
-    public void test() throws Exception{
-        
+    public void testGetIndexPage() throws Exception{
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Sign in")));
+    }
+
+    @Test
+    public void loginTest() throws Exception{
+        this.mockMvc.perform(get("/orderPizza"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/user/login"));
     }
 
 }
